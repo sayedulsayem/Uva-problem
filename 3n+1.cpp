ffec5de9ad7320e9b5e1d691559dc7470ec64b0c
sayedulsayem <sayedulsayem@gmail.com>
2017-03-18 01:40:24 +0600

    Add files via upload

---
+++ 3n+1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    long int n1,n2,cnt,tmp,maxi,f,l;
+    while(scanf("%ld%ld",&n1,&n2)!=EOF)
+    {
+        f=n1;
+        l=n2;
+        if((n1>0&&n1<1000000)&&(n2>0&&n2<1000000))
+        {
+            maxi=0;
+            if(n1>n2)
+                swap(n1,n2);
+            for(int i=n2; i>=n1; i--)
+            {
+                cnt=1;
+                tmp=i;
+                while(tmp!=1)
+                {
+                    if(tmp%2==0)
+                    {
+                        tmp=tmp/2;
+                        cnt++;
+                    }
+                    else
+                    {
+                        tmp=((3*tmp)+1);
+                        cnt++;
+                    }
+                }
+                if(cnt>maxi)
+                    maxi=cnt;
+            }
+            printf("%ld %ld %ld\n",f,l,maxi);
+        }
+    }
+    return 0;
+}
